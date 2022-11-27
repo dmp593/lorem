817a93d195363f42353a8bdcaaf5a04b1a55306a
Daniel Pinto <d.pinto@kigroup.de>
2022-11-27 14:21:23 +0000

    using @cache on app/dependencies.py

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,9 +1,12 @@
 import os
 
+from functools import cache
+
 from fastapi import Depends
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorCollection, AsyncIOMotorDatabase
 
 
+@cache
 def get_connection_string() -> str:
     driver: str = 'mongodb'
     username: str = os.environ['DB_USER']
@@ -14,10 +17,13 @@ def get_connection_string() -> str:
     return f"{driver}://{username}:{password}@{host}:{port}/"
 
 
+@cache
 def get_client(conn_str: str = Depends(get_connection_string)) -> AsyncIOMotorClient:
     # set a 5-second connection timeout
     return AsyncIOMotorClient(conn_str, serverSelectionTimeoutMS=5000)
 
+
+@cache
 def get_db(client: AsyncIOMotorClient = Depends(get_client), db_name: str = 'lorem') -> AsyncIOMotorDatabase:
     return client[db_name]
 
